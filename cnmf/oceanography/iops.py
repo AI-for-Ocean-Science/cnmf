60be54642ba8e17720ab8dd31c72d570ee2fd831
profxj <xavier@ucolick.org>
2023-12-07 05:42:17 -0800

    wip

--- cnmf/oceanography/iops.py
+++ cnmf/oceanography/iops.py
@@ -4,6 +4,10 @@ import numpy as np
 
 from oceancolor.hydrolight import loisel23
 from oceancolor.iop import cross
+from oceancolor.tara import io as tara_io
+from oceancolor.tara import spectra
+from oceancolor.utils import spectra as spec_utils 
+from oceancolor import water
 
 def prep_loisel23(iop:str, min_wv:float=400., sigma:float=0.05,
                   X:int=4, Y:int=0):
@@ -55,3 +59,70 @@ def prep_loisel23(iop:str, min_wv:float=400., sigma:float=0.05,
 
     # Return
     return spec_nw, mask, err, wave, Rs
+
+
+def tara_matched_to_l23(low_cut:float=400., high_cut:float=705., 
+                        X:int=4, Y:int=0,
+                        include_water:bool=False):
+    """ Generate Tara spectra matched to L23
+
+    Restricted on wavelength and time of cruise
+    as per Patrick Gray recommendations
+
+    Args:
+        low_cut (float, optional): low cut wavelength. Defaults to 400..
+        high_cut (float, optional): high cut wavelength. Defaults to 705..
+        X (int, optional): simulation scenario. Defaults to 4.
+        Y (int, optional): solar zenith angle used in the simulation.
+            Defaults to 0.
+
+    Returns:
+        tuple: wavelength grid, Tara spectra, L23 spectra
+    """
+
+    # Load up the data
+    l23_ds = loisel23.load_ds(X, Y)
+
+    # Load up Tara
+    print("Loading Tara..")
+    tara_db = tara_io.load_pg_db(expedition='Microbiome')
+
+    # Spectra
+    wv_nm, all_a_p, all_a_p_sig = spectra.spectra_from_table(tara_db)
+
+    # Wavelengths, restricted to > 400 nm
+    cut = (l23_ds.Lambda > low_cut) & (l23_ds.Lambda < high_cut)
+    l23_a = l23_ds.a.data[:,cut]
+
+    # Rebin
+    wv_grid = l23_ds.Lambda.data[cut]
+    tara_wv = np.append(wv_grid, [high_cut+5.]) - 2.5 # Because the rebinning is not interpolation
+    rwv_nm, r_ap, r_sig = spectra.rebin_to_grid(
+        wv_nm, all_a_p, all_a_p_sig, tara_wv)
+
+    # Add in water
+    if include_water:
+        print("Adding in water..")
+        df_water = water.load_rsr_gsfc()
+        a_w, _ =  spec_utils.rebin(df_water.wavelength.values, 
+                            df_water.aw.values, np.zeros_like(df_water.aw),
+                            wv_grid)
+
+        final_tara = r_ap+np.outer(np.ones(r_ap.shape[0]), a_w)
+    else:
+        final_tara = r_ap
+
+
+    # Polish Tara for PCA
+    bad = np.isnan(final_tara) | (final_tara <= 0.)
+    ibad = np.where(bad)
+
+    mask = np.ones(final_tara.shape[0], dtype=bool)
+    mask[np.unique(ibad[0])] = False
+
+    # Cut down: Aggressive but necessary
+    final_tara = final_tara[mask,:]
+
+
+    # Return
+    return wv_grid, final_tara, l23_a
