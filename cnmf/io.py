b9fdead5198fef3d78872ed962566bf904e0e141
profxj <xavier@ucolick.org>
2023-12-12 05:51:40 -0800

    fit chl

--- cnmf/io.py
+++ cnmf/io.py
@@ -15,6 +15,32 @@ def nmf_filename(nmf_fit:str, N_NMF:int, iop:str=None):
     nmf_file = outroot+'.npz'
     return nmf_file
 
+def save_nmf(outfile:str, M:np.ndarray, coeff:np.ndarray, spec:np.ndarray,
+    mask:np.ndarray, err:np.ndarray, wave:np.ndarray,
+    Rs:np.ndarray):
+    """
+    Save the NMF results to a file in npz format.
+
+    Args:
+        outfile (str): The path of the output file.
+        M (np.ndarray): The spatial components matrix.
+        coeff (np.ndarray): The temporal coefficients matrix.
+        spec (np.ndarray): The spectral components matrix.
+        mask (np.ndarray): The binary mask matrix.
+        err (np.ndarray): The error matrix.
+        wave (np.ndarray): The wavelength array.
+        Rs (np.ndarray): The spatial correlation matrix.
+    """
+    # Create output directory?
+    if not os.path.exists(os.path.dirname(outfile)):
+        os.makedirs(os.path.dirname(outfile))
+    # Save
+    np.savez(outfile, M=M, coeff=coeff,
+             spec=spec, mask=mask,
+             err=err, wave=wave, Rs=Rs)
+    print(f'Wrote: {outfile}')
+             
+
 def load_nmf(nmf_fit:str, N_NMF:int, iop:str=None):
     """ Load a NMF model
 
